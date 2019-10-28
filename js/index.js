da17c6de850a76c51b2a89f8c4da730467d4099c
Paulo Hericles <paulo.hericles.silva@gmail.com>
2019-10-27 22:36:33 -0300

    Finished

--- js/index.js
+++ js/index.js
@@ -1,8 +1,23 @@
+window.onscroll = function() {
+    scroll();
+};
+
 // Flip card
 $(".card").flip();
 
 
 // Accordion
 $( function() {
-    $( "#accordion" ).accordion();
+    $( "#accordion" ).accordion({
+        heightStyle: "content"
+    });
 } );
+
+// Scroll Bar
+function scroll() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+
+    document.getElementById("myBar").style.width = scrolled + "%";
+}
