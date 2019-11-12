1b96c3f539b0be97b926273e9b25fc93e75e6313
Paulo Hericles <paulo.hericles.silva@gmail.com>
2019-11-12 19:38:55 -0300

    unidade 3

---
+++ unidade3-v1/lib/configuracoes.js
@@ -0,0 +1,10 @@
+/**
+ * CONFIGURAÇÕES DA PEÇA
+ * LEIA COM ATENÇÃO
+ * 
+ * ALTERE CADA CONFIGURAÇÃO DE ACORDO COM O AMBIENTE QUE
+ * A PEÇA IRÁ RODAR
+ */
+
+isStreaming = false; //TRUE para streaming, FALSE para donwload 
+idPeca = "unidade1";   //identificacao da peca ex.: "si286" ATENÇÃO: respeite letras MAIUSCULAS E MINUSCULAS
